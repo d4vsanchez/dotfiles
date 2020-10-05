e641e12ac602f5371854d34b144e3453ec30341e
David Sánchez <d4vsanchez@gmail.com>
2020-10-05 08:58:36 -0500

    Add dependencies of PyEnv in the installation process

--- install/install-pyenv.sh
+++ install/install-pyenv.sh
@@ -3,8 +3,12 @@
 # I use PyEnv as I need to have multiple Python versions.
 # I'm sorry for that, hope one day I could be at latest without any hassle
 
-py_36=3.6.10
-py_37=3.7.7
+py_36=3.6.12
+py_37=3.7.9
+
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 exec $SHELL
