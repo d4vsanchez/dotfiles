d83947dbe804df1a8a4196cd0c0285f9a135dbba
David Sánchez <2999604+d4vsanchez@users.noreply.github.com>
2024-04-13 11:31:53 -0500

    Current neovim configuration

---
+++ .config/nvim/lua/plugins/nvim-notify.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      stages = "static",
+      -- reduce the notifications to only WARNING or higher priority
+      level = 3,
+      render = "minimal",
+    },
+  },
+}
